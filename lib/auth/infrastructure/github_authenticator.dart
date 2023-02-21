3f655f8fe2445fe4db0b357ca45cdc9886bde9b3
Dasun Sathsara <me@dasunsathsara.com>
2023-02-21 22:44:45 +0530

    Load client id and secret key from .env

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -5,6 +5,7 @@ import 'package:http/http.dart' as http;
 import 'package:oauth2/oauth2.dart';
 import 'package:repoviewer/auth/domain/auth_failure.dart';
 import 'package:repoviewer/core/shared/encoders.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'credentials_storage/credentials_storage.dart';
 
@@ -24,8 +25,8 @@ class GithubAuthenticator {
 
   GithubAuthenticator(this._credentialStorage, this._dio);
 
-  static const clientId = '';
-  static const clientSecret = '';
+  static final clientId = dotenv.env['CLIENT_ID'] ?? '';
+  static final clientSecret = dotenv.env['CLIENT_SECRET'] ?? '';
   static const scopes = ['read:user', 'repo'];
   static final authorizationEndpoint = Uri.parse('https://github.com/login/oauth/authorize');
   static final tokenEndpoint = Uri.parse('https://github.com/login/oauth/access_token');
